00f67d20bdffd285e1e402f2f04511fbeadcaf22
Amr Taha <amrshiha2@gmail.com>
2022-07-27 17:48:20 +0200

    changing data structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,19 @@ import { ChevronDownIcon } from "@heroicons/react/solid";
 import { TodoItem } from "./components/TodoItem";
 import dummyData from "./dummyData.json";
 
+type TodoType = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+type Todos = {
+  completed: TodoType[];
+  notCompleted: TodoType[];
+};
+
 function App() {
-  const [todos, setTodos] = useState(dummyData.todos);
+  const [todos, setTodos] = useState<Todos>(dummyData.todos);
 
   const toggleTodo = (ref: React.MutableRefObject<null>, id: number) => {
     const todo = ref.current! as HTMLDivElement;
@@ -18,37 +29,49 @@ function App() {
 
     setTimeout(() => {
       setTodos((prev) => {
-        return prev.map((todo) => {
-          if (todo.id === id) return { ...todo, completed: !todo.completed };
-          return todo;
-        });
+        if (prev.completed.find((todo) => todo.id === id)) {
+          const ourTodo = prev.completed.find((todo) => todo.id === id);
+          return {
+            completed: prev.completed.filter((todo) => todo.id !== id),
+            notCompleted: [
+              { id, title: ourTodo!.title, completed: false },
+              ...prev.notCompleted,
+            ],
+          };
+        } else {
+          const ourTodo = prev.notCompleted.find((todo) => todo.id === id);
+          return {
+            completed: [
+              { id, title: ourTodo!.title, completed: true },
+              ...prev.completed,
+            ],
+            notCompleted: prev.notCompleted.filter((todo) => todo.id !== id),
+          };
+        }
       });
     }, 500);
   };
 
   return (
     <div className="App space-y-2 mx-auto max-w-md">
-      {todos
-        .filter((todo) => !todo.completed)
-        .map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            toggleTodo={toggleTodo}
-            isCompleted={false}
-          />
-        ))}
+      {todos.notCompleted.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          toggleTodo={toggleTodo}
+          isCompleted={false}
+        />
+      ))}
 
-      <div className="p-4 flex space-x-2 text-gray-400">
-        <ChevronDownIcon className="h-5" />
-        <p className="text-sm">
-          Completed {todos.filter((todo) => todo.completed).length}
-        </p>
-      </div>
+      {todos.completed.length !== 0 && (
+        <div className="p-4 flex space-x-2 text-gray-400">
+          <ChevronDownIcon className="h-5" />
+          <p className="text-sm">Completed {todos.completed.length}</p>
+        </div>
+      )}
 
-      {todos
-        .filter((todo) => todo.completed)
-        .map((todo) => (
+      {todos.completed &&
+        todos.completed.map((todo) => (
           <TodoItem
             key={todo.id}
             todo={todo}
